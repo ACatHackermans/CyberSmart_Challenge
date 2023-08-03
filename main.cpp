fcdd1f11e0264178ffa84a9c81c02cd5d077d87b
ACatHackermans <136595822+ACatHackermans@users.noreply.github.com>
2023-08-03 12:39:29 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,46 +1,83 @@
 #include <SFML/Graphics.hpp>
+#include <cmath>
 
 using namespace sf;
 
+float calculateDistance (const Vector2f& point1, const Vector2f& point2)
+{
+    float distanceX = point2.x - point1.x;
+    float distanceY = point2.y - point1.y;
+    return sqrt(distanceX * distanceX + distanceY * distanceY);
+}
+void menu_button ()
+{
+
+}
+
 int main()
 {
-    bool exit = false;
+    RenderWindow window(VideoMode(1920, 1080), "CyberSmart Challenge", Style::Fullscreen);
 
-    RenderWindow window(VideoMode(1600, 900), "CyberSmart Challenge", Style::Fullscreen);
-    // Texture close_button;
-    // if (!close_button.loadFromFile("close_button.png"))
-    // {
-
-    // }
-
-    while (!exit)
+    Texture close_button, play_button, settings_button, info_button, back_button, background1;
+    if (!close_button.loadFromFile("resources/close_button.png") || !play_button.loadFromFile("resources/play_button.png") 
+    || !settings_button.loadFromFile("resources/settings_button.png") || !info_button.loadFromFile("resources/info_button.png")
+    || !back_button.loadFromFile("resources/back_button.png") || !background1.loadFromFile("resources/background1.png"))
     {
-        while (window.isOpen())
-        {    Event event;
-            while (window.pollEvent(event))
+        return 0;
+    }
+
+    Sprite closebutton(close_button), playbutton(play_button), settingsbutton(settings_button), infobutton(info_button), bg1(background1);
+    closebutton.setScale(0.15, 0.15);
+    closebutton.setPosition(Vector2f(100, 50));
+    playbutton.setScale(0.15, 0.15);
+    playbutton.setPosition(Vector2f(800, 500));
+    settingsbutton.setScale(0.15, 0.15);
+    settingsbutton.setPosition(Vector2f(100, 830));
+    infobutton.setScale(0.15, 0.15);
+    infobutton.setPosition(Vector2f(1520, 830));
+
+    while (window.isOpen())
+    {    
+        Event event;
+        Vector2f mousePosition = static_cast<Vector2f>(Mouse::getPosition(window));
+
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
             {
-                switch (event.type)
-                {
-                case Event::Closed:
+            case Event::Closed:
+                window.close();
+                break;
+
+            case Event::KeyPressed:
+                if (event.key.code == Keyboard::Escape)
+                {  
                     window.close();
-                    break;
-
-                case Event::KeyPressed:
-                    if (event.key.code == Keyboard::Escape)
-                    {   
-                        exit = true;
-                        window.close();
-                    }
-                    break;            
                 }
+                break;   
+
+            case Event::MouseButtonPressed:
+                if (event.mouseButton.button == Mouse::Left)
+                {
+                    float closestDistance = 50;
+                    if (calculateDistance(mousePosition, closebutton.getPosition()) < closestDistance)
+                    {
+                        return 0;
+                    }
+                }
+                    break;         
             }
-
-            window.clear();
-
-            //window.draw(close_button);
-
-            window.display();
         }
+
+        window.clear();
+
+        window.draw(bg1);
+        window.draw(closebutton);
+        window.draw(playbutton);
+        window.draw(settingsbutton);
+        window.draw(infobutton);
+
+        window.display();
     }
 
     return 0;
